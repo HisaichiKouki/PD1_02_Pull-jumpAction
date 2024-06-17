a92f3b128821bc100ea431371c2f2c1c48c52ca5
ヒサイチ_コウキ <k023g0068@g.neec.ac.jp>
2024-06-17 15:31:12 +0900

    移動床を実装

---
+++ Assets/BlockMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMove : MonoBehaviour
+{
+    public float moveTime;
+    public float moveSpeed;
+
+    private float moveCount;
+    private float moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveCount = 0;
+        moveDirection = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveCount += Time.deltaTime;
+        if (moveCount > moveTime)
+        {
+            moveCount = 0;
+            moveDirection *= -1;
+        }
+        Vector3 newPosition=transform.position;
+        newPosition.x += moveDirection * moveSpeed*Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
